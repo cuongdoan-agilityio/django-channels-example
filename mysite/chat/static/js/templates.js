956d1666e2312483bd2e42591cce2be80392df70
Cuong Doan <cuong.doan@asnet.com.vn>
2022-11-03 18:38:17 +0700

    Update the date-time render template.

--- mysite/chat/static/js/templates.js
+++ mysite/chat/static/js/templates.js
@@ -49,27 +49,63 @@ const renderDateTimePickerContent = (data) => {
   if (!data.answer) {
     return `
       <p class="mb-0">${data.message }</p>
-      <div class='date position-relative' id="${data.message_id}">
-        <div class="pt-4" id="date-picker-${data.message_id}">
+      <div class='date position-relative pt-4' id="${data.message_id}">
+        <p class="border-bottom">Choose A Date and Time:</p>
+        
+        <div class="pt-1" id="date-picker-${data.message_id}">
           <!-- date picker -->
           <input type="hidden" id="${data.message_id}-date">
           <div id="${data.message_id}-datepicker"></div>
 
-          <div class="pt-4 d-flex">
-            <!-- time -->
-            <input
-              class="form-control"
-              type="time"
-              id="${data.message_id}-time"
-              name="${data.message_id}-time"
-              required
-            >
-            <input
-              id="${data.message_id}-next"
-              class="btn btn-primary mx-2"
-              type="button"
-              value="Next"
-            >
+          <div class="pt-4">
+
+            <div class="d-flex justify-content-between">
+              <!-- time -->
+              <input
+                type="text"
+                maxlength="5"
+                placeholder="00:00"
+                class="time-input form-control"
+                onkeypress="return onlyNumber(event, this)"
+                id="${data.message_id}-time"
+                name="${data.message_id}-time"
+                onchange="setError(this, '${data.message_id}-error')"
+              />
+
+              <!-- AM/PM -->
+              <div class="btn-group btn-group-toggle radio-group ms-1" data-toggle="buttons">
+                <input
+                  type="radio"
+                  class="btn-check"
+                  name="${data.message_id}-meridian"
+                  id="${data.message_id}-am"
+                  autocomplete="off"
+                  value="AM"
+                  checked
+                >
+                <label class="btn btn-sm  radio-label" for="${data.message_id}-am">AM</label>
+                
+                <input
+                  type="radio"
+                  class="btn-check"
+                  name="${data.message_id}-meridian"
+                  id="${data.message_id}-pm"
+                  autocomplete="off"
+                  value="PM"
+                >
+                <label class="btn btn-sm radio-label" for="${data.message_id}-pm">PM</label>
+              </div>
+
+              <input
+                id="${data.message_id}-next"
+                class="btn btn-primary ms-1 btn-sm btn-next"
+                type="button"
+                value="Next"
+              >
+            </div>
+            <div class="error text-danger d-none" id="${data.message_id}-error">
+              Invalid time.
+            </div>
           </div>
         </div>
       </div>
